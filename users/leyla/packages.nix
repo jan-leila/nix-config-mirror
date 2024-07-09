b526c1850cc190fd3927d23ad6f075762c87f84f
Leyla Becker <git@jan-leila.com>
2024-07-09 18:19:34 -0500

    removed emulators

--- users/leyla/packages.nix
+++ users/leyla/packages.nix
@@ -73,28 +73,28 @@ in
       (lib.mkIf cfg.hasOpenRGBHardware openrgb)
       (lib.mkIf cfg.hasViaKeyboard via)
 
-      # gaming
-      # emulators
-      # nintendo
-      # TODO: replace this with self hosted flake
-      # (lib.mkIf cfg.hasGPU yuzu-mainline) # Switch Emulator
-      # TODO: replace this with self hosted flake
-      # citra-canary # 3DS emulator
-      (lib.mkIf cfg.hasGPU cemu) # Wii-U emulator
-      dolphin-emu # GameCube and Wii Emulator
-      desmume # DS Emulator
-      mupen64plus # N64 Emulator
-      zsnes # SNES Emulator
-      vbam # Game Boy Advanced Emulator
-      fceux # NES Emulator
-      # play station
-      rpcs3 # PS3 Emulator
-      pcsx2 # PS2 Emulator
-      pcsxr # PS1 Emulator
-      # TODO: more play station emulators here when they come out
-      #misc
-      stella # Atari 2600 Emulator
-      mame # mame Emulator
+      # # gaming
+      # # emulators
+      # # nintendo
+      # # TODO: replace this with self hosted flake
+      # # (lib.mkIf cfg.hasGPU yuzu-mainline) # Switch Emulator
+      # # TODO: replace this with self hosted flake
+      # # citra-canary # 3DS emulator
+      # (lib.mkIf cfg.hasGPU cemu) # Wii-U emulator
+      # dolphin-emu # GameCube and Wii Emulator
+      # desmume # DS Emulator
+      # mupen64plus # N64 Emulator
+      # zsnes # SNES Emulator
+      # vbam # Game Boy Advanced Emulator
+      # fceux # NES Emulator
+      # # play station
+      # rpcs3 # PS3 Emulator
+      # pcsx2 # PS2 Emulator
+      # pcsxr # PS1 Emulator
+      # # TODO: more play station emulators here when they come out
+      # #misc
+      # stella # Atari 2600 Emulator
+      # mame # mame Emulator
     ]
   );
 }
