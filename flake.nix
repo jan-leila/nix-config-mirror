3530ee07d7ae611657f5ce997762fb64659a5c7b
Leyla Becker <git@jan-leila.com>
2024-09-07 19:36:23 -0500

    organized flake.nix

--- flake.nix
+++ flake.nix
@@ -2,21 +2,30 @@
   description = "Nixos config flake";
 
   inputs = {
+    # base packages
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
+    # encrypt files that contain secreats that I would like to not encrypt
     sops-nix.url = "github:Mic92/sops-nix";
 
+    # managment per user
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    # repo of hardware configs for prebuilt systems
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
   outputs = { self, nixpkgs, nixos-hardware, ... }@inputs:
     let
-      forEachSystem = nixpkgs.lib.genAttrs [ "x86_64-linux" ];
+      forEachSystem = nixpkgs.lib.genAttrs [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
       forEachPkgs = lambda: forEachSystem (system: lambda nixpkgs.legacyPackages.${system});
     in
     {
