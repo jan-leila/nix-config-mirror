513cc95af77693e6e1c56ae9ef9afe49b282b2b8
Leyla Becker <git@jan-leila.com>
2024-09-18 09:40:41 -0500

    disabled overlay for jetbrains idea

--- overlays/intellij.nix
+++ overlays/intellij.nix
@@ -1,19 +1,19 @@
 _:
 {
-  nixpkgs.overlays = [
-    (self: super: {
-      # idea is too out of date for android gradle things
-      jetbrains = {
-        jdk = super.jdk17;
-        idea-community = super.jetbrains.idea-community.overrideAttrs (oldAttrs: rec {
-          version = "2023.3.3";
-          name = "idea-community-${version}";
-          src = super.fetchurl {
-            sha256 = "sha256-3BI97Tx+3onnzT1NXkb62pa4dj9kjNDNvFt9biYgP9I=";
-            url = "https://download.jetbrains.com/idea/ideaIC-${version}.tar.gz";
-          };
-        });
-      };
-    })
-  ];
+  # nixpkgs.overlays = [
+  #   (self: super: {
+  #     # idea is too out of date for android gradle things
+  #     jetbrains = {
+  #       jdk = super.jdk17;
+  #       idea-community = super.jetbrains.idea-community.overrideAttrs (oldAttrs: rec {
+  #         version = "2023.3.3";
+  #         name = "idea-community-${version}";
+  #         src = super.fetchurl {
+  #           sha256 = "sha256-3BI97Tx+3onnzT1NXkb62pa4dj9kjNDNvFt9biYgP9I=";
+  #           url = "https://download.jetbrains.com/idea/ideaIC-${version}.tar.gz";
+  #         };
+  #       });
+  #     };
+  #   })
+  # ];
 }
