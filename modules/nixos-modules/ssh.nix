6b1eaa730850b4e166820324ce68077e3bdae93c
Leyla Becker <git@jan-leila.com>
2024-12-13 18:53:15 -0600

    added authorized ssh key

--- modules/nixos-modules/ssh.nix
+++ modules/nixos-modules/ssh.nix
@@ -1,4 +1,12 @@
-{...}: {
+{
+  lib,
+  config,
+  ...
+}: {
+  users.users.leyla.openssh.authorizedKeys.keys = lib.mkIf config.host.users.leyla.isTerminalUser [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJHeItmt8TRW43uNcOC+eIurYC7Eunc0V3LGocQqLaYj leyla@horizon"
+  ];
+
   services = {
     openssh = {
       enable = true;
