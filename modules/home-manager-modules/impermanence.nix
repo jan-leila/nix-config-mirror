7127b9f9e8815100af86b00fe2126313b35beb28
Leyla Becker <git@jan-leila.com>
2024-12-26 23:05:23 -0600

    updated packages

--- modules/home-manager-modules/impermanence.nix
+++ modules/home-manager-modules/impermanence.nix
@@ -1,10 +1,17 @@
-{config, ...}: {
-  home.persistence."/persistent/home/${config.home.username}" = {
-    directories = [
-      ".ssh"
-      "desktop"
-      "downloads"
-      "documents"
-    ];
+{
+  lib,
+  config,
+  osConfig,
+  ...
+}: {
+  config = lib.mkIf osConfig.host.impermanence.enable {
+    home.persistence."/persistent/home/${config.home.username}" = {
+      directories = [
+        ".ssh"
+        "desktop"
+        "downloads"
+        "documents"
+      ];
+    };
   };
 }
