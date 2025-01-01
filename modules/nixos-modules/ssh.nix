22b40edb241063ce182629b4aa1f9ef34168c185
Leyla Becker <git@jan-leila.com>
2025-01-01 17:02:46 -0600

    fixed SSH on defiant

--- modules/nixos-modules/ssh.nix
+++ modules/nixos-modules/ssh.nix
@@ -19,8 +19,11 @@
     }
     (lib.mkIf config.host.impermanence.enable {
       environment.persistence."/persist/system/root" = {
-        directories = [
-          "/etc/ssh"
+        files = [
+          "/etc/ssh/ssh_host_ed25519_key"
+          "/etc/ssh/ssh_host_ed25519_key.pub"
+          "/etc/ssh/ssh_host_rsa_key"
+          "/etc/ssh/ssh_host_rsa_key.pub"
         ];
       };
     })
