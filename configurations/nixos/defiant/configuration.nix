46edb7363d64c5dfe8c2c1daf646312c23e03bcb
Leyla Becker <git@jan-leila.com>
2025-01-01 20:01:44 -0600

    disabled sleeping on defiant

--- configurations/nixos/defiant/configuration.nix
+++ configurations/nixos/defiant/configuration.nix
@@ -125,6 +125,14 @@
     };
   };
 
+  # disable computer sleeping
+  systemd.targets = {
+    sleep.enable = false;
+    suspend.enable = false;
+    hibernate.enable = false;
+    hybrid-sleep.enable = false;
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
